ba9e23f9c00928a26df30c66a0a663982a73a410
gbrllcavichion <cavichionmichel@gmail.com>
2023-05-04 18:37:01 -0300

    adding consulta and new migrations

---
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -0,0 +1,16 @@
+package med.voll.api.domain.consulta;
+
+import jakarta.validation.constraints.*;
+
+import java.time.*;
+
+public record DadosAgendamentoConsulta(
+        Long idMedico,
+
+        @NotNull
+        Long idPaciente,
+
+        @NotNull
+        @Future
+        LocalDateTime data) {
+}
