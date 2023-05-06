366b180dc331bfee9b4e0520a5d13bcda4b0b413
gbrllcavichion <cavichionmichel@gmail.com>
2023-05-06 15:32:23 -0300

    Agenda de Consultas

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,6 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.validation.*;
 import med.voll.api.domain.consulta.*;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
 import org.springframework.transaction.annotation.*;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("consultas")
 public class ConsultaController {
+
+    @Autowired
+    private AgendaDeConsultas agenda;
 
     @PostMapping
     @Transactional
