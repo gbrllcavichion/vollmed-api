84c7bf63d045cc9b6ddaeb5780a8d1a13d0fe922
gbrllcavichion <cavichionmichel@gmail.com>
2023-05-06 15:47:08 -0300

    agenda service

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,12 +1,28 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.medico.*;
+import med.voll.api.domain.paciente.*;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 
 @Service
 public class AgendaDeConsultas {
 
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
     public void agendar(DadosAgendamentoConsulta dados) {
 
+        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+        var consulta = new Consulta(null, medico, paciente, dados.data());
+        consultaRepository.save(consulta);
     }
 
 }
