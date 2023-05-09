f8324aaed1d7f1ccb772709ed93c1613cd63b8ff
gbrllcavichion <cavichionmichel@gmail.com>
2023-05-09 10:52:46 -0300

    validation method

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -1,10 +1,21 @@
 package med.voll.api.domain.consulta.validacoes;
 
+import med.voll.api.domain.*;
 import med.voll.api.domain.consulta.*;
+
+import java.time.*;
 
 public class ValidadorHorarioFuncionamentoClinica {
 
     public void validar(DadosAgendamentoConsulta dados) {
+        var dataConsulta = dados.data();
 
+        var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var antesDaAberturaDaClinica = dataConsulta.getHour() < 7;
+        var depoisDoEncerramentoDaClinica = dataConsulta.getHour() > 18;
+
+        if (domingo || antesDaAberturaDaClinica || depoisDoEncerramentoDaClinica) {
+            throw new ValidacaoException("Consulta fora do horario de funcionamento da clínica");
+        }
     }
 }
