b28a91297e2d1e5243c18e10aee862da70e59325
gbrllcavichion <cavichionmichel@gmail.com>
2023-05-09 10:48:41 -0300

    creating validation class

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -0,0 +1,10 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.*;
+
+public class ValidadorHorarioFuncionamentoClinica {
+
+    public void validar(DadosAgendamentoConsulta dados) {
+
+    }
+}
