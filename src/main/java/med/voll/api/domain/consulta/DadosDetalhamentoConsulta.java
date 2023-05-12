461cc8b210f66fe45c30b55b522b23cb3463e9a2
gbrllcavichion <cavichionmichel@gmail.com>
2023-05-12 18:00:58 -0300

    mudando o retorno do metodo

--- src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
@@ -3,4 +3,7 @@ package med.voll.api.domain.consulta;
 import java.time.*;
 
 public record DadosDetalhamentoConsulta(Long id, Long idMedico, Long idPaciente, LocalDateTime data) {
+    public DadosDetalhamentoConsulta(Consulta consulta) {
+        this(consulta.getId(), consulta.getMedico().getId(), consulta.getPaciente().getId(), consulta.getData());
+    }
 }
