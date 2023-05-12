33b631a939fcfec68a1a3b2076136b5b960e7c07
gbrllcavichion <cavichionmichel@gmail.com>
2023-05-12 18:11:00 -0300

    var dto

--- src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
+++ src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
@@ -2,6 +2,8 @@ package med.voll.api.domain.usuario;
 
 import org.springframework.data.jpa.repository.*;
 import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.*;
+
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     UserDetails findByLogin(String login);
